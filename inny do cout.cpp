97571ff432676102223fe8ebbc333eacc735988b
kajo100@gmail.com <kajo100@gmail.com@f5324198-4ced-ebec-c7ac-02d811ed9d46>
2012-03-09 18:29:41 +0000

    added new & some modifies

---
+++ inny do cout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout<<"hehe :D ciekawe jakie taby bedizemy orbic";
+	
+	if (alfa > 0)
+		alfa++;
+	
+	return 0;
+}
--- spr.cpp
+++ spr.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-  return 0;
+	cout<<"dodamy tutaj cos";
+	for (;;) {
+		cout<<i++;
+		break;
+	}
+	return 0;
 }
