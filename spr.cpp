af77009e716c186cbee85883d4e519555ce09f3c
kajo100@gmail.com <kajo100@gmail.com@f5324198-4ced-ebec-c7ac-02d811ed9d46>
2012-03-28 21:36:45 +0000

    gdgd

--- spr.cpp
+++ spr.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main() {
 	cout<<"dodamy tutaj cos";
-	for (int i=0; i<10; i++) {
+	for (int i=520; i<10; i++) {
 		//cout<<i++;
+<<<<<<< .mine
+		if ( konflikt then ) end
+		form exec java c
+=======
 		
 		// nie nie, tu cos ci nie pyklo kajo
 		// powinno byc:
 		cout << i++ << endl;
+>>>>>>> .r9
 	}
 	return 0;
 }
