95d22a3bd9ddc6af023b490b981f3d98ce7cb7df
miloszos@gmail.com <miloszos@gmail.com@f5324198-4ced-ebec-c7ac-02d811ed9d46>
2012-03-28 21:28:24 +0000

    Kluczowe zmiany

--- spr.cpp
+++ spr.cpp
@@ -4,6 +4,10 @@ int main() {
 	cout<<"dodamy tutaj cos";
 	for (int i=0; i<10; i++) {
 		//cout<<i++;
+		
+		// nie nie, tu cos ci nie pyklo kajo
+		// powinno byc:
+		cout << i++ << endl;
 	}
 	return 0;
 }
