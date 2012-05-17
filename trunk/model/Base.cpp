1f67c2ff1520fde2440051cdc17f841ea0bd441c
kajo100@gmail.com <kajo100@gmail.com@f5324198-4ced-ebec-c7ac-02d811ed9d46>
2012-05-17 22:21:31 +0000

    - zrobilem segment - bezier itp    tam popraw private protected itp w tym bo nie pamietam jak jest poprawnie - dziala teraz - dalem przeciazenie << zeby tych pointsow co ciagle musze obczajac nie wypisywac recznie - przymierzam sie do parsowania obiektow - trzeba klasy uprzatnac w nich tam obiektow symulacji

--- trunk/model/Base.cpp
+++ trunk/model/Base.cpp
@@ -5,4 +5,11 @@ namespace zpr
 	Point::Point(double x, double y) : x_(x), y_(y)
 	{
 	}
+
+	
+	std::ostream& operator<<(std::ostream &os, Point &point)
+	{
+		os<<"("<<point.x_<<", "<<point.y_<<")";
+		return os;
+	}
 }
