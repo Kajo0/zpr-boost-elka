20e1d85e49017cdb367a7c5a73fb20e9ae9cd241
kajo100 <kajo100@gmail.com>
2012-05-18 12:12:30 +0000

    - parsowanie obiektow

--- controller/Controller.cpp
+++ controller/Controller.cpp
@@ -24,7 +24,7 @@ namespace zpr
 		////parseMap("xml_data\\streets.xml");
 		////model_.streets_.printAllData();
 
-
+		parseObjects("xml_data\\objects.xml");
 	}
 
 	void Controller::parseDispatcher(const char *path)
@@ -100,6 +100,81 @@ namespace zpr
 						//edges.push_back(std::make_pair(from, to));
 
 						model_.streets_.addEdge(from, to);
+					}
+				}
+			}
+		}	// TODO sprytnie jakos obsluzyc te exceptiony - moze lapac je w loopie i pytac o poprawienie albo o poprawna sciezke lub czy uruchomic bez kamer
+		catch (boost::property_tree::xml_parser_error&)
+		{
+			Logger::getInstance().message("No files with map !!");
+		}
+		catch (boost::property_tree::ptree_bad_path&)
+		{
+			Logger::getInstance().message("Error while reading tree nodes: node doesn't exist !");
+		}
+		catch (std::exception &e)
+		{
+			Logger::getInstance().message(e.what());
+		}
+	}
+
+	void Controller::parseObjects(const char *path)
+	{
+		using boost::property_tree::ptree;
+		ptree pt;
+		Model::PCar car;
+		Model::PWalker walker;
+	
+		try
+		{
+			read_xml(path, pt, boost::property_tree::xml_parser::no_comments);
+
+			BOOST_FOREACH( ptree::value_type &v, pt.get_child("objects") )
+			{
+				if ( v.second.get<std::string>("<xmlattr>.type") == "car" )
+				{
+					std::string id = v.second.get<std::string>("id");
+					double weight = v.second.get<double>("parameters.weight");
+					std::string size = v.second.get<std::string>("parameters.size"); // potem enum jakis
+					double mspeed = v.second.get<double>("parameters.maxspeed");
+
+					//car.reset( new xCar(...) );
+					//model_.cars_.insert( std::pair<std::string, Model::PCar>( id, car ) );
+
+					std::cout<<v.second.get<std::string>("<xmlattr>.type")
+						<<" id: "<<id<<" weight: "<<weight<<" size: "<<size<<" speed: "<<mspeed<<"\n";
+
+
+					BOOST_FOREACH( ptree::value_type &vv, v.second.get_child("track") )
+					{
+						Point point( vv.second.get<double>("<xmlattr>.x"),
+									vv.second.get<double>("<xmlattr>.y") );
+
+						std::cout<<"\t"<<point<<"\n";
+						
+						//model_.cars_[car->registration_]->track_.addPoint(); lub stworzenie tutaj tracka i na konice go dodanie
+					}
+				}
+				else if ( v.second.get<std::string>("<xmlattr>.type") == "walker" )
+				{
+					std::string id = v.second.get<std::string>("id");
+					double mspeed = v.second.get<double>("parameters.maxspeed");
+
+					//walker.reset( new Walker(...) );
+					//model_.walkers_.insert( std::pair<std::string, Model::PWalker>( id, walker ) );
+
+					std::cout<<v.second.get<std::string>("<xmlattr>.type")
+						<<" id: "<<id<<" speed: "<<mspeed<<"\n";
+
+
+					BOOST_FOREACH( ptree::value_type &vv, v.second.get_child("track") )
+					{
+						Point point( vv.second.get<double>("<xmlattr>.x"),
+									vv.second.get<double>("<xmlattr>.y") );
+
+						std::cout<<"\t"<<point<<"\n";
+						
+						//model_.walkers_[walker->id_]->track_.addPoint(); lub stworzenie tutaj tracka i na konice go dodanie
 					}
 				}
 			}
