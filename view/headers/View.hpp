f8bd39ffb5236f22c13b44f9c55449c43e724d64
kajo100 <kajo100@gmail.com>
2012-05-20 22:31:28 +0000

    - view dalem jakiegos frame'a   on przejmuje watek maina, bo to tytlko test - ogolnie chce wczytac te samochody i inne i je powyswietlac zeby spr to ale czyt komenty - modle sie zeby do w ogole dzialalo potem na unixie ! - link allegro 5 z tutoriala plug into visual 2010 gdzies na stronie allegro wiki

--- view/headers/View.hpp
+++ view/headers/View.hpp
@@ -1,11 +1,20 @@
 #ifndef VIEW_HPP
 #define VIEW_HPP
 
+#include <allegro5/allegro.h>
+
 namespace zpr
 {
 	class View
 	{
+		ALLEGRO_DISPLAY *display_; // tu mamy nasz frame i wsio chyba w jednym
+		ALLEGRO_EVENT_QUEUE *eventQueue_; // tu eventy typu tez X on close
 
+		public:
+		View();
+		virtual ~View();
+
+		void loop();
 	};
 }
 #endif // VIEW_HPP
